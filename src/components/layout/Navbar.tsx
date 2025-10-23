279ada095c2c3757ea58c1e1d86612b06971b5fc
Laxmi Narashima <laxminarashima@gmail.com>
2025-10-23 19:24:26 +0530

    removed odd even page

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -59,8 +59,7 @@ const Navbar: React.FC = () => {
               </Link>
             )}
             <Link to="/" className="text-gray-700 hover:text-blue-600 transition-colors">Home</Link>
-            <Link to="/semesters?term=odd" className="text-gray-700 hover:text-blue-600 transition-colors">Odd Sem</Link>
-            <Link to="/semesters?term=even" className="text-gray-700 hover:text-blue-600 transition-colors">Even Sem</Link>
+            
             <Link to="/resources" className="text-gray-700 hover:text-blue-600 transition-colors">Resources</Link>
             {currentUser && (
               <Link to="/upload" className="flex items-center space-x-1 text-gray-700 hover:text-blue-600 transition-colors">
